1f2844f28351581b5a4be8d534d27e57e0ba8db2
Ryan Djurovich <git@ryandjurovich.com>
2014-06-27 14:41:25 +1000

    Used mock in ServiceFactoryTest

--- Evolution7/SocialApi/Tests/Service/ServiceFactoryTest.php
+++ Evolution7/SocialApi/Tests/Service/ServiceFactoryTest.php
@@ -2,19 +2,26 @@
 
 namespace Evolution7\SocialApi\Tests\Service;
 
-use Evolution7\SocialApi\Config\Config;
 use Evolution7\SocialApi\Service\ServiceFactory;
+use Evolution7\SocialApi\Config\ConfigInterface;
 
 class ServiceFactoryTest extends \PHPUnit_Framework_TestCase
 {
 
+    private function getConfigMock($platform)
+    {
+        $config = $this->getMock('Evolution7\SocialApi\Config\ConfigInterface');
+        $config->expects($this->any())
+               ->method('getPlatform')
+               ->will($this->returnValue($platform));
+        return $config;
+    }
+
     public function testCreate()
     {
-        $config = new Config('instagram', 'a', 'b', array(), 'c');
-        $result = ServiceFactory::get($config);
+        $result = ServiceFactory::get($this->getConfigMock('instagram'));
         $this->assertInstanceOf('\Evolution7\SocialApi\Service\Instagram', $result);
-        $config = new Config('twitter', 'a', 'b', array(), 'c');
-        $result = ServiceFactory::get($config);
+        $result = ServiceFactory::get($this->getConfigMock('twitter'));
         $this->assertInstanceOf('\Evolution7\SocialApi\Service\Twitter', $result);
     }
 
@@ -23,8 +30,7 @@ class ServiceFactoryTest extends \PHPUnit_Framework_TestCase
      */
     public function testCreateNotExist()
     {
-        $config = new Config('notexist', 'a', 'b', array(), 'c');
-        ServiceFactory::get($config);
+        ServiceFactory::get($this->getConfigMock('notexist'));
     }
     
 }
