4b40a25788ffd74222c32a819e2e37d4e6a0235b
Ryan Djurovich <git@ryandjurovich.com>
2014-06-30 17:26:39 +1000

    Fixed twitter search

--- Evolution7/SocialApi/Service/Twitter.php
+++ Evolution7/SocialApi/Service/Twitter.php
@@ -3,6 +3,7 @@
 namespace Evolution7\SocialApi\Service;
 
 use Evolution7\SocialApi\Exception\NotImplementedException;
+use Evolution7\SocialApi\ApiPost\TwitterPost;
 
 class Twitter extends Service implements ServiceInterface
 {
@@ -23,17 +24,14 @@ class Twitter extends Service implements ServiceInterface
         $responseArray = json_decode($responseRaw, true);
         // Create return array
         $return = array();
-        // Check if value returned
-        if (array_key_exists('value', $responseArray)) {
-            // Check if statuses exist
-            if (array_key_exists('statuses', $responseArray['value'])) {
-                // Check if at least one statuses exists
-                if (count($responseArray['value']['statuses']) > 0) {
-                    // Loop statuses
-                    foreach ($responseArray['value']['statuses'] as $status) {
-                        // Create new TwitterPost object and add to return array
-                        $return[] = new TwitterPost(json_encode($status));
-                    }
+        // Check if statuses exist
+        if (array_key_exists('statuses', $responseArray)) {
+            // Check if at least one statuses exists
+            if (count($responseArray['statuses']) > 0) {
+                // Loop statuses
+                foreach ($responseArray['statuses'] as $status) {
+                    // Create new TwitterPost object and add to return array
+                    $return[] = new TwitterPost(json_encode($status));
                 }
             }
         }
