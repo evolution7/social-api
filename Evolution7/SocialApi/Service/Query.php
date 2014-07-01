79b893ec267107ef2fb58cc0d4a195a1af20d0cf
Ryan Djurovich <git@ryandjurovich.com>
2014-07-01 17:04:41 +1000

    Added limit to query

--- Evolution7/SocialApi/Service/Query.php
+++ Evolution7/SocialApi/Service/Query.php
@@ -5,6 +5,12 @@ namespace Evolution7\SocialApi\Service;
 class Query implements QueryInterface
 {
     private $hashtags;
+    private $media;
+    private $idFrom;
+    private $idTo;
+    private $dateFrom;
+    private $dateTo;
+    private $numResults;
 
     /**
      * Constructor
@@ -44,6 +50,108 @@ class Query implements QueryInterface
     public function filterByHashtags($hashtags)
     {
         $this->hashtags = array_merge($this->hashtags, array_flip($hashtags));
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getMedia()
+    {
+        return array_keys($this->media);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function filterByMedia($media)
+    {
+        $this->media = array_merge($this->media, array_flip($hashtags));
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getIdFrom()
+    {
+        return $this->idFrom;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function limitIdFrom($id)
+    {
+        $this->idFrom = $id;
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getIdTo()
+    {
+        return $this->idTo;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function limitIdTo($id)
+    {
+        $this->idTo = $id;
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getDateFrom()
+    {
+        return $this->dateFrom;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function limitDateFrom(DateTime $date)
+    {
+        $this->dateFrom = $date;
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getDateTo()
+    {
+        return $this->dateTo;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function limitDateTo(DateTime $date)
+    {
+        $this->dateTo = $date;
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getNumResults()
+    {
+        return $this->dateFrom;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function limitNumResults($numResults)
+    {
+        $this->numResults = $numResults;
         return $this;
     }
 }
--- Evolution7/SocialApi/Service/QueryInterface.php
+++ Evolution7/SocialApi/Service/QueryInterface.php
@@ -35,4 +35,100 @@ interface QueryInterface
      * @return QueryInterface
      */
     public function filterByHashtags($hashtags);
+
+    /**
+     * Get media to filter by
+     *
+     * @return array
+     */
+    public function getMedia($state);
+
+    /**
+     * Filter by media e.g. images
+     *
+     * @param array $media
+     *
+     * @return QueryInterface
+     */
+    public function filterByMedia($media);
+
+    /**
+     * Get from id to filter by
+     *
+     * @return DateTime
+     */
+    public function getIdFrom();
+
+    /**
+     * Set from id to filter by
+     *
+     * @param int $id
+     *
+     * @return QueryInterface
+     */
+    public function limitIdFrom($id);
+
+    /**
+     * Get to id to filter by
+     *
+     * @return int
+     */
+    public function getIdTo();
+
+    /**
+     * Set to id to filter by
+     *
+     * @param int $id
+     *
+     * @return QueryInterface
+     */
+    public function limitIdTo($id);
+
+    /**
+     * Get from date to filter by
+     *
+     * @return DateTime
+     */
+    public function getDateFrom();
+
+    /**
+     * Set from date to filter by
+     *
+     * @param DateTime $date
+     *
+     * @return QueryInterface
+     */
+    public function limitDateFrom(DateTime $date);
+
+    /**
+     * Get to date to filter by
+     *
+     * @return DateTime
+     */
+    public function getDateTo();
+
+    /**
+     * Set to date to filter by
+     *
+     * @param DateTime $date
+     *
+     * @return QueryInterface
+     */
+    public function limitDateTo(DateTime $date);
+
+    /**
+     * Get number results to limit to
+     *
+     * @return DateTime
+     */
+    public function getNumResults();
+
+    /**
+     * Set number results to limit to
+     *
+     * @param int $numResults
+     *
+     * @return QueryInterface
+     */
+    public function limitNumResults($numResults);
 }
