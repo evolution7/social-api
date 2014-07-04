e01fdd773c18e03686fc3c1b8680b6277055e87b
Ryan Djurovich <git@ryandjurovich.com>
2014-07-04 15:19:00 +1000

    Extended testParseUserArray and testParsePostArray

--- Evolution7/SocialApi/Tests/Parser/InstagramParserTest.php
+++ Evolution7/SocialApi/Tests/Parser/InstagramParserTest.php
@@ -61,8 +61,17 @@ class InstagramParserTest extends Parser
         $response = new Response($sampleResponse, 'json');
         // Get parser
         $parser = new InstagramParser($response);
-        // Parse and test
+        // Parse data
         $user = $parser->parseUserArray($response->getArray());
+        // Test output
+        $this->assertInstanceOf('\Evolution7\SocialApi\Entity\User', $user);
+        $this->assertEquals('1574083', $user->getId());
+        $this->assertEquals('snoopdogg', $user->getHandle());
+        $this->assertEquals('Snoop Dogg', $user->getName());
+        $this->assertEquals(
+            'http://distillery.s3.amazonaws.com/profiles/profile_1574083_75sq_1295469061.jpg',
+            $user->getImageUrl()
+        );
     }
 
     public function testParsePostArray()
@@ -72,7 +81,25 @@ class InstagramParserTest extends Parser
         $response = new Response($sampleResponse, 'json');
         // Get parser
         $parser = new InstagramParser($response);
-        // Parse and test
-        $user = $parser->parsePostArray($response->getArray());
+        // Parse data
+        $post = $parser->parsePostArray($response->getArray());
+        // Test output
+        $this->assertInstanceOf('\Evolution7\SocialApi\Entity\Post', $post);
+        $this->assertEquals('3', $post->getId());
+        $this->assertEquals('Hello world', $post->getBody());
+        $this->assertEquals(
+            'http://distillery.s3.amazonaws.com/media/2010/07/16/4de37e03aa4b4372843a7eb33fa41cad_7.jpg',
+            $post->getMediaUrl()
+        );
+        $dateTime = new \DateTime();
+        $dateTime->setTimestamp(1279340983);
+        $this->assertEquals($dateTime, $post->getCreated());
+        // Test user
+        $user = $post->getUser();
+        $this->assertInstanceOf('\Evolution7\SocialApi\Entity\User', $user);
+        $this->assertEquals('3', $user->getId());
+        $this->assertEquals('kevin', $user->getHandle());
+        $this->assertEquals('Kevin S', $user->getName());
+        $this->assertEquals('...', $user->getImageUrl());
     }
 }
